ff2c6c1381bbda60d2546b0e415d089cc62718a0
David Tirabassi <davtir@hotmail.com>
2026-03-17 14:33:12 +1100

    feat(roadmap): explicitly size text column & remove add column

--- public/js/ui/ganttView.js
+++ public/js/ui/ganttView.js
@@ -266,11 +266,10 @@ const GanttView = (() => {
             gantt.config.grid_resize = true; // Enable resizing the grid area
 
             gantt.config.columns = [
-                {name:"text", label:"Task name", tree:true, width:'*', resize:true },
+                {name:"text", label:"Task name", tree:true, width:250, resize:true },
                 {name:"start_date", label:"Start Time", align: "center", resize:true },
                 {name:"duration", label:"Duration", align: "center", resize:true },
-                {name:"dependentTeam", label:"Dependent Team", align: "center", resize:true },
-                {name:"add", label:"", width: 44}
+                {name:"dependentTeam", label:"Dependent Team", align: "center", resize:true }
             ];
             gantt.config.readonly = true;
             gantt.config.fit_tasks = true; // Ensure tasks fit when changing view
